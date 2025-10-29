9a612db415d0b35093a0646d2a3ced6bbaafc61f
dev <dev@finalyards.xyz>
2025-10-29 19:02:48 +0200

    101

--- ci.sh
+++ ci.sh
@@ -15,39 +15,33 @@ if [[ -z "${CARGO_TARGET_DIR}" ]]; then
     export CARGO_TARGET_DIR=target_ci
 fi
 
-cargo batch \
-    --- check --release --manifest-path host/Cargo.toml --no-default-features --features peripheral \
-    --- check --release --manifest-path host/Cargo.toml --no-default-features --features central \
-    --- check --release --manifest-path host/Cargo.toml --no-default-features --features central,scan \
-    --- check --release --manifest-path host/Cargo.toml --no-default-features --features central,peripheral \
-    --- check --release --manifest-path host/Cargo.toml --no-default-features --features central,peripheral,defmt \
-    --- check --release --manifest-path host/Cargo.toml --no-default-features --features gatt,peripheral \
-    --- check --release --manifest-path host/Cargo.toml --no-default-features --features gatt,central \
-    --- check --release --manifest-path host/Cargo.toml --no-default-features --features gatt,peripheral,central,scan \
-    --- check --release --manifest-path host/Cargo.toml --no-default-features --features gatt,peripheral,central,scan,security \
-    --- check --release --manifest-path host/Cargo.toml --no-default-features --features gatt,peripheral,central,scan,controller-host-flow-control \
-    --- check --release --manifest-path host/Cargo.toml --no-default-features --features gatt,peripheral,central,scan,controller-host-flow-control,connection-metrics,channel-metrics \
-    --- check --release --manifest-path host/Cargo.toml --no-default-features --features gatt,peripheral,central,scan,controller-host-flow-control,connection-metrics,channel-metrics,l2cap-sdu-reassembly-optimization \
-    --- check --release --manifest-path host/Cargo.toml --no-default-features --features gatt,peripheral,central,scan,controller-host-flow-control,connection-metrics,channel-metrics,l2cap-sdu-reassembly-optimization,connection-params-update \
-    --- check --release --manifest-path bt-hci-linux/Cargo.toml \
-    --- check --release --manifest-path examples/nrf52/Cargo.toml --target thumbv7em-none-eabihf --features nrf52840 \
-    --- check --release --manifest-path examples/nrf52/Cargo.toml --target thumbv7em-none-eabihf --features nrf52840,security \
-    --- check --release --manifest-path examples/nrf52/Cargo.toml --target thumbv7em-none-eabihf --features nrf52833 --artifact-dir tests/nrf52 \
-    --- check --release --manifest-path examples/nrf52/Cargo.toml --target thumbv7em-none-eabihf --features nrf52832 \
-    --- check --release --manifest-path examples/esp32/Cargo.toml --target riscv32imc-unknown-none-elf --features esp32c3 --artifact-dir tests/esp32 \
-    --- check --release --manifest-path examples/serial-hci/Cargo.toml \
-    --- check --release --manifest-path examples/linux/Cargo.toml \
-    --- check --release --manifest-path examples/linux/Cargo.toml --features security \
-    --- check --release --manifest-path examples/tests/Cargo.toml \
-    --- check --release --manifest-path benchmarks/nrf-sdc/Cargo.toml --target thumbv7em-none-eabihf --features nrf52840 \
-    --- check --release --manifest-path examples/rp-pico-w/Cargo.toml --target thumbv6m-none-eabi --features skip-cyw43-firmware \
-    --- check --release --manifest-path examples/rp-pico-2-w/Cargo.toml --target thumbv8m.main-none-eabihf --features skip-cyw43-firmware
-#    --- check --release --manifest-path examples/apache-nimble/Cargo.toml --target thumbv7em-none-eabihf
-    #
-    # NOTE! Later 'cargo' can do 'check --locked' but 'cargo-batch' doesn't allow it!
-    #       >> error: unexpected argument '--locked' found
-    #
-    #     This might be one reason to move from 'cargo-batch' to regular, line-wise 'cargo'.
+cargo check --release --manifest-path host/Cargo.toml --no-default-features --features peripheral
+cargo check --release --manifest-path host/Cargo.toml --no-default-features --features central
+cargo check --release --manifest-path host/Cargo.toml --no-default-features --features central,scan
+cargo check --release --manifest-path host/Cargo.toml --no-default-features --features central,peripheral
+cargo check --release --manifest-path host/Cargo.toml --no-default-features --features central,peripheral,defmt
+cargo check --release --manifest-path host/Cargo.toml --no-default-features --features gatt,peripheral
+cargo check --release --manifest-path host/Cargo.toml --no-default-features --features gatt,central
+cargo check --release --manifest-path host/Cargo.toml --no-default-features --features gatt,peripheral,central,scan
+cargo check --release --manifest-path host/Cargo.toml --no-default-features --features gatt,peripheral,central,scan,security
+cargo check --release --manifest-path host/Cargo.toml --no-default-features --features gatt,peripheral,central,scan,controller-host-flow-control
+cargo check --release --manifest-path host/Cargo.toml --no-default-features --features gatt,peripheral,central,scan,controller-host-flow-control,connection-metrics,channel-metrics
+cargo check --release --manifest-path host/Cargo.toml --no-default-features --features gatt,peripheral,central,scan,controller-host-flow-control,connection-metrics,channel-metrics,l2cap-sdu-reassembly-optimization
+cargo check --release --manifest-path host/Cargo.toml --no-default-features --features gatt,peripheral,central,scan,controller-host-flow-control,connection-metrics,channel-metrics,l2cap-sdu-reassembly-optimization,connection-params-update
+cargo check --release --manifest-path bt-hci-linux/Cargo.toml
+cargo check --release --manifest-path examples/nrf52/Cargo.toml --target thumbv7em-none-eabihf --features nrf52840
+cargo check --release --manifest-path examples/nrf52/Cargo.toml --target thumbv7em-none-eabihf --features nrf52840,security
+cargo check --release --manifest-path examples/nrf52/Cargo.toml --target thumbv7em-none-eabihf --features nrf52833 --artifact-dir tests/nrf52
+cargo check --release --manifest-path examples/nrf52/Cargo.toml --target thumbv7em-none-eabihf --features nrf52832
+cargo check --release --manifest-path examples/esp32/Cargo.toml --target riscv32imc-unknown-none-elf --features esp32c3 --artifact-dir tests/esp32
+cargo check --release --manifest-path examples/serial-hci/Cargo.toml
+cargo check --release --manifest-path examples/linux/Cargo.toml
+cargo check --release --manifest-path examples/linux/Cargo.toml --features security
+cargo check --release --manifest-path examples/tests/Cargo.toml
+cargo check --release --manifest-path benchmarks/nrf-sdc/Cargo.toml --target thumbv7em-none-eabihf --features nrf52840
+cargo check --release --manifest-path examples/rp-pico-w/Cargo.toml --target thumbv6m-none-eabi --features skip-cyw43-firmware
+cargo check --release --manifest-path examples/rp-pico-2-w/Cargo.toml --target thumbv8m.main-none-eabihf --features skip-cyw43-firmware
+# cargo check --release --manifest-path examples/apache-nimble/Cargo.toml --target thumbv7em-none-eabihf
 
 set -x
 cargo fmt --check --manifest-path ./host/Cargo.toml
